3fc8ce435ac641f7aa139ada36c0d2a89d335172
Rutuja <rutuja1675@gmail.com>
2022-07-06 22:46:34 +0530

    two-way bind

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -2,57 +2,43 @@ import "./NewExpenseForm.css";
 import { useState } from "react";
 
 const NewExpenseForm = () => {
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  // in update, the state is dependent on the previous one
-  // and the first commented approach my cause cause problems
-  // (react schedules its state updates, incase you schedule a lot
-  // of state updates at the same time, you could be depending on
-  // an outdated or an incorrect state snapshot if this approach is used.
-  // If the next one with prevState is used, react guarantees it gives you
-  // the latest state snapshots keeping all scheduled state updates in mind)
-  // therefore, use an alternative way of including a function
-  // in which prevState is thecallback and we return the updated value
   const titleChangeHandler = (event) => {
-    // setUserInput({
-    //     ...userInput,
-    //     enteredTitle: event.target.value
-    // })
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: event.target.value };
-    });
+    setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: event.target.value,
-    // });
-    setUserInput((prevState) => {
-      return { ...prevState, enteredAmount: event.target.value };
-    });
+    setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: event.target.value,
-    // });
-    setUserInput((prevState) => {
-      return { ...prevState, enteredDate: event.target.value };
-    });
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+
+    //state helps in clearing the form values after submission along with value={entered...}
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
@@ -61,6 +47,7 @@ const NewExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -71,6 +58,7 @@ const NewExpenseForm = () => {
             type="date"
             min="2022-01-01"
             max="2025-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
