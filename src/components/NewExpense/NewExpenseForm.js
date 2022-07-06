ce74b3c5e228f431281469de3cfb44b58b5fdd6c
Rutuja <rutuja1675@gmail.com>
2022-07-06 18:06:22 +0530

    listen to input (multistate soln)

--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -1,22 +1,51 @@
 import "./NewExpenseForm.css";
+import { useState } from "react";
 
 const NewExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //useState is used for storing the entered title here primarily
+  //and not to update the component which by its virtue happens anyways
+  const titleChangeHandler = event => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-01-01" max="2025-12-31" />
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2025-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
 
