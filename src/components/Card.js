b7486d25327d6bab35552864e73cd8f864e0817a
Rutuja <rutuja1675@gmail.com>
2022-07-06 13:45:24 +0530

    compose components

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+// This type of component is basically a shell ie a container surrounding some other component
+// aka wrapper component, for this to work around customized html/jsx components,
+// we output default props.children (contains all the content in the wrapper)
+import "./Card.css";
+
+function Card(props) {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
