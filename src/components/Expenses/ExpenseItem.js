6a2274f87aa172dcdced32ecfd74eba614c4a862
Rutuja <rutuja1675@gmail.com>
2022-07-06 15:56:25 +0530

    show no change on click

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,29 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card.js";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = "Updated!";
+    //here the title won't get updated because 
+    //react has already rendered the app, and 
+    //changes in such viariables or an event 
+    //occuring (a click here) does not trigger 
+    //the reavluation and rerendering of that component 
+    //ergo comes the concept of state
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      {/* onClick is a prop provided by Reactjs */}
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
