a10bc01498b2b268dc1ceb618b51ad14e78df56e
Rutuja <rutuja1675@gmail.com>
2022-07-21 09:03:01 +0530

    use effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // following creates an infinte loop as the component reloads after setIsLoggedIn is called
+  // const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+  // if (storedUserLoggedInInfo) setIsLoggedIn(true);
+
+  //we don't use localStorage directly but inside the hook, useEffect() to prevent the side effect
+  //this runs after every component's re-evalation and then only if there's change in its dependencies
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInfo) setIsLoggedIn(true);
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </>
   );
 }
 
