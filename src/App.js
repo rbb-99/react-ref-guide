a77c51eb71eab3e48a2c1a65c82508ff412e0a5a
Rutuja <rutuja1675@gmail.com>
2022-07-21 14:21:53 +0530

    relocate context authentication to a file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
-import React, { useEffect, useState } from "react";
-import AuthContext from "./store/auth-context";
-
+import React, { useContext } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
-    if (storedUserLoggedInInfo) setIsLoggedIn(true);
-  }, []);
-
-  const loginHandler = (email, password) => {
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
-    setIsLoggedIn(false);
-  };
-
+  const ctx = useContext(AuthContext);
   return (
     <>
-      <AuthContext.Provider
-        value={{
-          isLoggedIn: isLoggedIn,
-          onLogout: logoutHandler
-        }}
-      >
-        <MainHeader />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </AuthContext.Provider>
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
     </>
   );
 }
