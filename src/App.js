a7ad5c5d5f526d9eb4d08a5dc22ec3366f075172
Rutuja <rutuja1675@gmail.com>
2022-07-27 17:04:38 +0530

    connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -7,41 +8,63 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-/* The React useCallback Hook returns a memoized callback function.
-Think of memoization as caching a value so that it does not need to be recalculated.
-This allows us to isolate resource intensive functions so that they will not automatically run on every render.
-The useCallback Hook only runs when one of its dependencies update.This can improve performance.
-The useCallback and useMemo Hooks are similar. The main difference is that useMemo returns a memoized value 
-and useCallback returns a memoized function. You can learn more about useMemo in the useMemo chapter.
-One reason to use useCallback is to prevent a component from re-rendering unless its props have changed. */
-
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const responsePromise = await fetch("https://swapi.dev/api/films");
+      const responsePromise = await fetch(
+        "https://react-http-ed4ad-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!responsePromise.ok) {
         throw new Error("Something went wrong!");
       }
+
       const data = await responsePromise.json();
-      const transformedMovies = data.results.map((movieObject) => {
-        return {
-          id: movieObject.episode_id,
-          title: movieObject.title,
-          openingText: movieObject.opening_crawl,
-          releaseDate: movieObject.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }, []); // here, we add external dependencies but there aren't any (setError, setMovies,... are handled internally)
+  }, []);
 
   useEffect(() => {
     fetchMoviesHandler();
-  }, []);
+  }, [fetchMoviesHandler]);
+
+  const addMovieHandler = async (movie) => {
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-ed4ad-default-rtdb.firebaseio.com/movies.json",
+        {
+          method: "POST",
+          body: JSON.stringify(movie),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong when getting data from Firebase");
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   let content = <p>Found no movies.</p>;
 
@@ -59,6 +82,9 @@ One reason to use useCallback is to prevent a component from re-rendering unless
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
