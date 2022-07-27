3a232aaf15dda2089b9aeabafa81b13cccf2afdc
Rutuja <rutuja1675@gmail.com>
2022-07-27 09:39:21 +0530

    handle http errors

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  //still an asynchronous code
   const fetchMoviesHandler = async () => {
-    const responsePromise = await fetch("https://swapi.dev/api/films");
-    const data = await responsePromise.json();
-    const transformedMovies = data.results.map((movieObject) => {
-      return {
-        id: movieObject.episode_id,
-        title: movieObject.title,
-        openingText: movieObject.opening_crawl,
-        releaseDate: movieObject.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setIsLoading(true);
+    setError(null);
+    try {
+      const responsePromise = await fetch("https://swapi.dev/api/films");
+      if (!responsePromise.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await responsePromise.json();
+      const transformedMovies = data.results.map((movieObject) => {
+        return {
+          id: movieObject.episode_id,
+          title: movieObject.title,
+          openingText: movieObject.opening_crawl,
+          releaseDate: movieObject.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
   };
 
   return (
@@ -26,7 +37,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>No movies found.</p>}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
