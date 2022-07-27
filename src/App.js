15edc8ef49f4d1b084525ab4c9f91628ed32b5c1
Rutuja <rutuja1675@gmail.com>
2022-07-27 10:14:38 +0530

    handle http req as side effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+
+// when user land s on the page the content should be loaded. 
+// for this use useEffect hook because sending this http request 
+// is a side effect which ultimately changes our component's state;
+// having side effects in a function is also fine as long as you don't 
+// call this function as part pf your main component function (here, App)
+// because then there can be an infinite loop where you call this function,
+// it updates the state, the component function re-renders/is re-evaluated
+// and the component function is called again ... Hence, useEffect()
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  // with the following we render it only once (initially)
+  useEffect(()=>{
+    fetchMoviesHandler()
+  }, [])
+  // now we want to add fetchMoviesHandler as a dependency 
+  // because any changes in it must be re-evaluated
+  // useEffect(()=>{
+  //   fetchMoviesHandler()
+  // }, [fetchMoviesHandler])
+  // but the above gets into an infinite loop, so we use callBack()
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
