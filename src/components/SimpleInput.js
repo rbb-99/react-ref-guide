3400a389d7fe42bd08e980f3d244b33acfcce987
Rutuja <rutuja1675@gmail.com>
2022-07-29 13:21:57 +0530

    input by state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,27 +1,30 @@
-// to fetch an input value, 2 ways:
-// 1. listen on every keystroke and store the value in some state variable
-// 2. use a ref to fetch the input once the user is done typing in a value
-// in reality we use one, but here try both
-
-//start with ref
-import { useRef } from "react";
+//now by state
+import { useState } from "react";
 
 const SimpleInput = () => {
-  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
-    //although this is not preferred in react as we're directly manipulating DOM wwhich should be left to react
-    nameInputRef.current.value = "";
+    console.log(enteredName);
+    // ideal reset
+    setEnteredName("");
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" ref={nameInputRef} />
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
