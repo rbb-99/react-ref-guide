d15aa4bca74fd0c54d9077bb70ee7455f1bfb2b0
Rutuja <rutuja1675@gmail.com>
2022-07-31 09:32:59 +0530

    dispatch actions from within functional components

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { legacy_createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = legacy_createStore(counterReducer);
+
+//now connect the App to store so those components can subscribe to the store
+export default store;
