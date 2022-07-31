88af979645d4f546c1e31ab63eaad2696591a6af
Rutuja <rutuja1675@gmail.com>
2022-07-31 16:17:41 +0530

    init commit: redux with food-order-app

---
+++ src/store/cart-slice.js
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    // additem action needs an object as a payload
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      state.totalQuantity++;
+
+      // if the new item there isn't already in the cart, add it to the
+      // cart and set it's quantity to 1 and totalPrice to it's price
+      // else increase the quantity by 1 and add to totalPrice
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
+
+    // removeItem needs an id for the payload
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
