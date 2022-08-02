a087ba0b318bcba2117d2e192fb43ed9107c04f1
Rutuja <rutuja1675@gmail.com>
2022-08-02 17:07:36 +0530

    build for production

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = "https://react-http-ed4ad-default-rtdb.firebaseio.com/";
+const FIREBASE_DOMAIN = "https://react-prep-c520c-default-rtdb.firebaseio.com/";
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
