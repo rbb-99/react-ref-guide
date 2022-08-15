19463e108c0d1a0d603b5bda56c72a3ec3b2f3eb
Rutuja <rutuja1675@gmail.com>
2022-08-15 17:57:11 +0530

    protect frontend pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/profile" element={<UserProfile />} />
+        {!authCtx.isLoggedIn && <Route path="/auth" element={<AuthPage />} />}
+        <Route path="/profile">
+          {authCtx.isLoggedIn && <UserProfile />}{" "}
+          {!authCtx.isLoggedIn && <Navigate to="/auth" replace />}
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Layout>
   );
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 import { useContext } from "react";
 import AuthContext from "../../store/auth-context";
@@ -6,7 +6,6 @@ import AuthContext from "../../store/auth-context";
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn;
-  const navigate = useNavigate();
 
   const logoutHandler = () => {
     authCtx.logout();
