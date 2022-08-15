06153ebe786fad7eed0c01777ad3071f7b42983b
Rutuja <rutuja1675@gmail.com>
2022-08-15 16:52:04 +0530

    manage auth state

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
@@ -7,6 +8,8 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const passInputRef = useRef();
   const [isLoading, setIsLoading] = useState(false);
+
+  const authCtx = useContext(AuthContext);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -42,7 +45,8 @@ const AuthForm = () => {
       });
       if (res.ok) {
         const data = await res.json();
-        console.log(data)
+        // console.log(data);
+        authCtx.login(data.idToken);
       } else {
         const errData = await res.json();
         console.log(errData);
---
+++ src/store/auth-context.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  //if token is a string that's not empty, it'll return true
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logoutHandler: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
