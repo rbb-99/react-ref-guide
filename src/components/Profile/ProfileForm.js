6127c17d80ba08cd13019c435ee500dd98e4980e
Rutuja <rutuja1675@gmail.com>
2022-08-15 17:21:08 +0530

    use token to access protected resource

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,64 @@
-import classes from './ProfileForm.module.css';
+import { useRef, useContext, useState } from "react";
+import AuthContext from "../../store/auth-context";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const authCtx = useContext(AuthContext);
+  const newPassRef = useRef();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const enteredNewPass = newPassRef.current.value;
+    //add validation
+
+    setIsLoading(true);
+    try {
+      const res = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAZf9M0_LmpCrKxRxNmWbrKjJ-FCBbcRj0",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: authCtx.token,
+            password: enteredNewPass,
+            returnSecureToken: false,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        // authCtx.login(data.idToken);
+      } else {
+        const errData = await res.json();
+        console.log(errData);
+        let errMsg = "Authentication failed";
+        if (errData && errData.error && errData.error.message) {
+          errMsg = errData.error.message;
+        }
+        throw new Error(errMsg);
+      }
+    } catch (error) {
+      alert(error);
+    }
+    setIsLoading(false);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input ref={newPassRef} type="password" id="new-password" />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        {!isLoading && <button>Change Password</button>}
+        {isLoading && <p>Requesting...</p>}
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
