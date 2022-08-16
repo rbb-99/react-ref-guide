d188e99679c78bd261358107e8059eead234e2fe
Rutuja <rutuja1675@gmail.com>
2022-08-16 11:46:59 +0530

    extract dynamic parameter

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,13 @@
 // domain.com/news/newsId
 
+import { useRouter } from "next/router";
+
 const NewsDetails = () => {
+  const router = useRouter();
+  console.log(router.query.newsId);
+
+  // send a request to backend API to fetch news item with newsId
+
   return <h1>NewsDetails</h1>;
 };
 
