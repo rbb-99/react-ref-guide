0b74b950062cc147093a9fd4ffc9d0c041e1e8ae
Rutuja <rutuja1675@gmail.com>
2022-08-16 11:39:36 +0530

    add dynamic pages

---
+++ pages/news/[newsId].js
@@ -0,0 +1,7 @@
+// domain.com/news/newsId
+
+const NewsDetails = () => {
+  return <h1>NewsDetails</h1>;
+};
+
+export default NewsDetails;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,4 @@
 // domain.com/news
-import React from "react";
 
 const News = () => {
   return <h1>News</h1>;
